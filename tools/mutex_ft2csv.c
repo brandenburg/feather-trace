57e0f63308063b5e092bca23d05978e30742d361
Bjoern Brandenburg <bbb@mpi-sws.org>
2012-03-29 11:36:21 +0200

    Clean up mutex_ft2csv
    This tool extracts .csv files from .ft files collected with
    the LD_PRELOAD lock tracing proxy.
    
    It supports two modes. Normally, it extracts the critical
    section lengths. If given the -d option, it will simply
    convert the binary event data into a .csv file for further
    processing.

--- tools/mutex_ft2csv.c
+++ tools/mutex_ft2csv.c
@@ -24,8 +24,8 @@ int map_file(const char* filename, void **addr, size_t *size)
 		if (info.st_size > 0) {
 			fd = open(filename, O_RDWR);
 			if (fd >= 0) {
-				*addr = mmap(NULL, *size, 
-					     PROT_READ | PROT_WRITE, 
+				*addr = mmap(NULL, *size,
+					     PROT_READ | PROT_WRITE,
 					     MAP_PRIVATE,
 					     fd, 0);
 				if (*addr == MAP_FAILED)
@@ -47,7 +47,7 @@ struct timestamp {
 	void* lock;
 	long thread;
 	int nesting;
-	unsigned long long timestamp;	
+	unsigned long long timestamp;
 };
 
 
@@ -55,42 +55,36 @@ static unsigned int incomplete = 0;
 static unsigned int complete   = 0;
 static unsigned int filtered   = 0;
 
-// same theshold as in ospert paper (?)
-static unsigned long long threshold = 2700 * 100;
-//static unsigned long long threshold = 200000;
-//static unsigned long long threshold = 2700000000ULL; /* one second */
-
-static struct timestamp* next(struct timestamp** pos, size_t* count, int cpu)
+static struct timestamp* next(struct timestamp** pos, size_t* count, long cpu)
 {
 	while (*count--)
 		if ((++(*pos))->thread == cpu)
-			return *pos;       
+			return *pos;
 	return NULL;
 }
 
-static struct timestamp* next_id(struct timestamp** pos, size_t* count, 
-				 int cpu, unsigned long id, 
-				 void* lock) 
+static struct timestamp* next_id(struct timestamp** pos, size_t* count,
+				 long cpu, unsigned long id,
+				 void* lock)
 {
 	struct timestamp* ret;
-	while ((ret = next(pos, count, cpu)) && 
+	while ((ret = next(pos, count, cpu)) &&
 	       ((*pos)->event != id || (*pos)->lock != lock));
-	return ret; 
+	return ret;
 }
-
 
 static int check(struct timestamp* lock, struct timestamp* unlock)
 {
 	struct timestamp* pos = lock;
 	while (++pos != unlock)
-		if (pos->event == lock->event && 
+		if (pos->event == lock->event &&
 		    pos->thread == lock->thread &&
 		    pos->lock == lock->lock)
 			return 0;
-	return 1;		
+	return 1;
 }
 
-static int find_pair(struct timestamp* start, 
+static int find_pair(struct timestamp* start,
 		     struct timestamp** end,
 		     size_t count)
 {
@@ -107,51 +101,28 @@ static void show_csv(struct timestamp* ts, size_t count)
 	struct timestamp* stop = NULL;
 
 	if (find_pair(start, &stop, count)) {
-		if (stop->timestamp - start->timestamp >= threshold)
-			filtered++;
-		else {
-			printf("%llu, %llu, %llu, %u, %ld\n",
-			       start->timestamp, stop->timestamp, 
-			       stop->timestamp - start->timestamp,
-			       start->nesting, start->thread);
-			complete++;
-		}
+		printf("%llu, %llu, %llu, %u, %ld\n",
+		       start->timestamp, stop->timestamp,
+		       stop->timestamp - start->timestamp,
+		       start->nesting, start->thread);
+		complete++;
 	} else {
-//		fprintf(stderr, "0x%x by %d\n", ts->lock, ts->ctx.pid);
 		if (stop)
-			fprintf(stderr, 
+			fprintf(stderr,
 				"missmatch %p locked by %ld, "
-				"released by %ld, check=%d\n", 
+				"released by %ld, check=%d\n",
 				ts->lock, ts->thread, stop->thread,
 				check(start, stop));
 		else
-			fprintf(stderr, " 2nd missing: %p locked by %ld\n",
-				ts->lock, ts->thread);
+			fprintf(stderr, " 2nd missing: %p locked by 0x%lx at %10llu\n",
+				ts->lock, ts->thread, ts->timestamp);
 
 		incomplete++;
 	}
-	
 }
 
-/*static void show_all_per_cpu(struct timestamp* ts, size_t count)
-{
-	struct timestamp* _ts = ts;
-	size_t _count = count;
-	int cpu;
-	
-	for (cpu = 0; cpu < 4; cpu++) {
-		count = _count;
-		ts    = _ts;
-		while (count--) {
-			if (ts->cpu == cpu)
-				show(ts, count);	
-			ts++;
-		}
-	}
-}*/
 
-
-static int  preskip(struct timestamp** ts, size_t *count, unsigned long id)
+int  preskip(struct timestamp** ts, size_t *count, unsigned long id)
 {
 	int skip = 0;
 	while (*count)
@@ -168,17 +139,11 @@ static int  preskip(struct timestamp** ts, size_t *count, unsigned long id)
 
 static void show_id(struct timestamp* ts, size_t count,  unsigned long id)
 {
-	/* skip bad first entries, only stuff recorded after the 
-	 * finish event went live is reliable.
-	 */
-	fprintf(stderr, 
-		"skipped   : %d\n",
-		preskip(&ts, &count, id));
 	while (count--)
 		if (ts->event == id)
 			show_csv(ts++, count);
 		else
-			ts++;		
+			ts++;
 }
 
 
@@ -186,70 +151,99 @@ static void nest(struct timestamp* ts, size_t count, unsigned long id)
 {
 	struct timestamp* exit;
 	struct timestamp* pos;
-	
+
 	if (ts->event == id) {
 		if (find_pair(ts, &exit, count)) {
-			if (exit->timestamp - ts->timestamp >= threshold)
-				return;
 			pos = ts;
 			while (++pos != exit) {
 				if (pos->thread == ts->thread)
-					pos->nesting = 
+					pos->nesting =
 						ts->nesting + 1;
 			}
 		}
 	}
 }
 
-static void fill_in_nesting(struct timestamp* ts, size_t count, 
+static void fill_in_nesting(struct timestamp* ts, size_t count,
 			    unsigned long id)
 {
 	size_t c = count;
 	struct timestamp* ts_ = ts;
 	while (count--)
-		(ts++)->nesting = 0;	
+		(ts++)->nesting = 0;
 	count = c;
 	ts = ts_;
 	preskip(&ts, &count, id);
 	while (count--)
-		nest(ts++, count, id);	
+		nest(ts++, count, id);
 }
 
 
-static void die(char* msg)
+static void dump(struct timestamp* ts, size_t count)
+{
+	while (count--) {
+		printf("%6lu, %p, 0x%lx, %d, %10llu\n", ts->event, ts->lock,
+		       ts->thread, ts->nesting, ts->timestamp);
+		ts++;
+	}
+}
+
+static void usage(char* msg)
 {
 	if (errno)
 		perror("error: ");
-	fprintf(stderr, "%s\n", msg);	
+	fprintf(stderr, "%s\n", msg);
+	fprintf(stderr, "mutex_ft2csv [-d] FILE\n");
 	exit(1);
 }
 
-int main(int argc, char** argv) 
+#define OPTSTR "d"
+
+int main(int argc, char** argv)
 {
 	void* mapped;
 	size_t size, count;
-	struct timestamp* ts;	
-	unsigned long id = 1000;
+	struct timestamp* ts;
+	unsigned long id = 1000; /* default: LOCK_EVENT = 1000 */
 
-	if (argc != 2)
-		die("Usage: ft2csv  <logfile>");
-	if (map_file(argv[1], &mapped, &size))
-		die("could not map file");
+	int opt;
+	int want_dump = 0;
 
+	while ((opt = getopt(argc, argv, OPTSTR)) != -1) {
+		switch (opt) {
+		case 'd':
+			want_dump = 1;
+			break;
+		case ':':
+			usage("Argument missing.");
+			break;
+		case '?':
+		default:
+			usage("Bad argument.");
+			break;
+		}
+	}
+
+	if (map_file(argv[optind], &mapped, &size))
+		usage("could not map file");
 
 	ts    = (struct timestamp*) mapped;
-	count = size / sizeof(struct timestamp);		
+	count = size / sizeof(struct timestamp);
 
-	fprintf(stderr, "Extracting from %s...\n", argv[1]);
+	fprintf(stderr, "# Extracting from %s...\n", argv[optind]);
 	fill_in_nesting(ts, count, id);
-	show_id(ts, count, id);
 
-	fprintf(stderr, 
-		"Complete  : %d\n"
-		"Incomplete: %d\n"
-		"Filtered  : %d\n", 
-		complete,
-		incomplete, filtered);
+	if (want_dump)
+		dump(ts, count);
+	else {
+		show_id(ts, count, id);
+		fprintf(stderr,
+			"# Complete  : %d\n"
+			"# Incomplete: %d\n"
+			"# Filtered  : %d\n",
+			complete,
+			incomplete, filtered);
+	}
 
 	return 0;
 }
