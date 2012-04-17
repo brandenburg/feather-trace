00713b878636867ce07291c588509b38fa5bf152
Bjoern Brandenburg <bbb@mpi-sws.org>
2012-04-17 09:35:15 +0200

    Remove unneeded moves in x86_64 trigger
    This mirrors the previous patch for x86_32: by explicitly allocating
    the parameters in the registers needed for the calling sequence (and
    which will be clobbered anyway), register pressure is reduced and some
    move instructions can be omitted.

--- include/feather_trace_x86_64.h
+++ include/feather_trace_x86_64.h
@@ -37,10 +37,32 @@
  * x86_64 caller only owns rbp, rbx, r12-r15;
  * the callee can freely modify the others.
  */
-#define __FT_CLOBBER_LIST	"memory", "cc", "rdi", "rsi", "rdx", "rcx", \
+#define __FT_CLOBBER_LIST0	"memory", "cc", "rdi", "rsi", "rdx", "rcx", \
 			"r8", "r9", "r10", "r11", "rax"
 
-#define __FT_ARG(x) "r" ((long) (x))
+#define __FT_CLOBBER_LIST1	"memory", "cc", "rdi", "rdx", "rcx", \
+			"r8", "r9", "r10", "r11", "rax"
+
+#define __FT_CLOBBER_LIST2	"memory", "cc", "rdi", "rcx", \
+			"r8", "r9", "r10", "r11", "rax"
+
+#define __FT_CLOBBER_LIST3	"memory", "cc", "rdi", \
+			"r8", "r9", "r10", "r11", "rax"
+
+/* The registers RDI, RSI, RDX, RCX, R8 and R9 are used for integer and pointer
+ * arguments. */
+
+/* RSI */
+#define __FT_TMP1(x) "=S" (x)
+#define __FT_ARG1(x) "0" ((long) (x))
+
+/* RDX */
+#define __FT_TMP2(x) "=d" (x)
+#define __FT_ARG2(x) "1" ((long) (x))
+
+/* RCX */
+#define __FT_TMP3(x) "=c" (x)
+#define __FT_ARG3(x) "2" ((long) (x))
 
 #define ft_event(id, callback)                                  \
         __asm__ __volatile__(                                   \
@@ -48,7 +70,7 @@
 	    " call " #callback "                          \n\t" \
             __FT_EVENT_TABLE(id,1b,2f)				\
             "2:                                           \n\t" \
-        : : : __FT_CLOBBER_LIST)
+        : : : __FT_CLOBBER_LIST0)
 
 #define ft_event0(id, callback)                                 \
         __asm__ __volatile__(                                   \
@@ -57,40 +79,49 @@
 	    " call " #callback "                          \n\t" \
 	    __FT_EVENT_TABLE(id,1b,2f)				\
             "2:                                           \n\t" \
-        : :  : __FT_CLOBBER_LIST)
+        : :  : __FT_CLOBBER_LIST0)
 
 #define ft_event1(id, callback, param)                          \
+	do {							\
+		long __ft_tmp1;					\
 	__asm__ __volatile__(                                   \
 	    "1: jmp 2f                                    \n\t" \
-	    " movq %0, %%rsi				  \n\t"	\
 	    " movq $" #id ", %%rdi			  \n\t" \
 	    " call " #callback "                          \n\t" \
 	    __FT_EVENT_TABLE(id,1b,2f)				\
 	    "2:                                           \n\t" \
-	: : __FT_ARG(param)  : __FT_CLOBBER_LIST)
+	    : __FT_TMP1(__ft_tmp1)				\
+	    : __FT_ARG1(param)					\
+	    : __FT_CLOBBER_LIST1);				\
+	} while (0);
 
 #define ft_event2(id, callback, param, param2)                  \
+	do {							\
+		long __ft_tmp1, __ft_tmp2;			\
         __asm__ __volatile__(                                   \
             "1: jmp 2f                                    \n\t" \
-	    " movq %1, %%rdx				  \n\t"	\
-	    " movq %0, %%rsi				  \n\t"	\
 	    " movq $" #id ", %%rdi			  \n\t" \
 	    " call " #callback "                          \n\t" \
             __FT_EVENT_TABLE(id,1b,2f)				\
             "2:                                           \n\t" \
-        : : __FT_ARG(param), __FT_ARG(param2)  : __FT_CLOBBER_LIST)
+	    : __FT_TMP1(__ft_tmp1), __FT_TMP2(__ft_tmp2)	\
+	    : __FT_ARG1(param), __FT_ARG2(param2)		\
+	    : __FT_CLOBBER_LIST2);				\
+	} while (0);
 
-#define ft_event3(id, callback, p, p2, p3)                      \
+#define ft_event3(id, callback, param, param2, param3)		\
+	do {							\
+		long __ft_tmp1, __ft_tmp2, __ft_tmp3;		\
         __asm__ __volatile__(                                   \
             "1: jmp 2f                                    \n\t" \
-	    " movq %2, %%rcx				  \n\t"	\
-	    " movq %1, %%rdx				  \n\t"	\
-	    " movq %0, %%rsi				  \n\t"	\
 	    " movq $" #id ", %%rdi			  \n\t" \
 	    " call " #callback "                          \n\t" \
             __FT_EVENT_TABLE(id,1b,2f)				\
             "2:                                           \n\t" \
-        : : __FT_ARG(p), __FT_ARG(p2), __FT_ARG(p3)  : __FT_CLOBBER_LIST)
+	    : __FT_TMP1(__ft_tmp1), __FT_TMP2(__ft_tmp2), __FT_TMP3(__ft_tmp3) \
+	    : __FT_ARG1(param), __FT_ARG2(param2), __FT_ARG3(param3)	\
+	    : __FT_CLOBBER_LIST3);				\
+	} while (0);
 
 static inline unsigned long long ft_read_tsc(void)
 {
