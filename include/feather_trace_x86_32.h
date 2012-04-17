225d7348a08682cd87f72b127142bdfd6c0c7890
Bjoern Brandenburg <bbb@mpi-sws.org>
2012-04-17 08:44:10 +0200

    Switch to regparm(3) on x86_32
    The old trigger code was unintentionally creating register pressure by
    forcing the parameters into registers that were NOT clobbered as part
    of a cdecl call. This is counter to out intentions: we want to use
    EAX, EDX, and ECX whenever possible since those don't have to be saved
    by the callee.
    
    As a nice side effect, the switch to regparm(3) also allows us to
    remove most of the stack setup/cleanup code.

--- include/feather_trace_x86_32.h
+++ include/feather_trace_x86_32.h
@@ -23,14 +23,23 @@
 
 /* Do not directly include this file. Include feather_trace.h instead */
 
-#define feather_callback __attribute__((regparm(0)))  __attribute__((used))
+#define feather_callback __attribute__((regparm(3)))  __attribute__((used))
 
 /*
- * make the compiler reload any register that is not saved in
- * a cdecl function call
+ * Make the compiler reload any register that is not saved in a cdecl function
+ * call (minus the registers that we explicitly clobber as output registers).
  */
-#define __FT_CLOBBER_LIST "memory", "cc", "eax", "ecx", "edx"
-#define __FT_ARG(x) "r" ((long) (x))
+#define __FT_CLOBBER_LIST0 "memory", "cc", "eax", "edx", "ecx"
+#define __FT_CLOBBER_LIST1 "memory", "cc", "eax", "ecx"
+#define __FT_CLOBBER_LIST2 "memory", "cc", "eax"
+#define __FT_CLOBBER_LIST3 "memory", "cc", "eax"
+
+#define __FT_TMP1(x) "=d" (x)
+#define __FT_ARG1(x) "0" ((long) (x))
+#define __FT_TMP2(x) "=c" (x)
+#define __FT_ARG2(x) "1" ((long) (x))
+
+#define __FT_ARG3(x) "r" ((long) (x))
 
 #define ft_event(id, callback)                                  \
         __asm__ __volatile__(                                   \
@@ -40,66 +49,70 @@
             ".long " #id  ", 0, 1b, 2f                    \n\t" \
             ".previous                                    \n\t" \
             "2:                                           \n\t" \
-        : : : __FT_CLOBBER_LIST)
+	    : : : __FT_CLOBBER_LIST0)
 
 #define ft_event0(id, callback)                                 \
         __asm__ __volatile__(                                   \
             "1: jmp 2f                                    \n\t" \
+            " movl $" #id  ", %%eax                       \n\t" \
+	    " call " #callback "                          \n\t" \
+            ".section __event_table, \"aw\"               \n\t" \
+            ".long " #id  ", 0, 1b, 2f                    \n\t" \
+            ".previous                                    \n\t" \
+            "2:                                           \n\t" \
+	    : : : __FT_CLOBBER_LIST0)
+
+#define ft_event1(id, callback, param)				\
+	do {							\
+		long __ft_tmp1;					\
+        __asm__ __volatile__(                                   \
+            "1: jmp 2f                                    \n\t" \
+            " movl $" #id  ", %%eax                       \n\t" \
+	    " call " #callback "                          \n\t" \
+            ".section __event_table, \"aw\"               \n\t" \
+            ".long " #id  ", 0, 1b, 2f                    \n\t" \
+            ".previous                                    \n\t" \
+            "2:                                           \n\t" \
+	    : __FT_TMP1(__ft_tmp1)				\
+	    : __FT_ARG1(param)					\
+	    : __FT_CLOBBER_LIST1);				\
+	} while (0);
+
+#define ft_event2(id, callback, param, param2)                  \
+	do {							\
+		long __ft_tmp1, __ft_tmp2;			\
+        __asm__ __volatile__(                                   \
+            "1: jmp 2f                                    \n\t" \
+            " movl $" #id  ", %%eax                       \n\t" \
+	    " call " #callback "                          \n\t" \
+            ".section __event_table, \"aw\"               \n\t" \
+            ".long " #id  ", 0, 1b, 2f                    \n\t" \
+            ".previous                                    \n\t" \
+            "2:                                           \n\t" \
+	    : __FT_TMP1(__ft_tmp1), __FT_TMP2(__ft_tmp2)	\
+	    : __FT_ARG1(param), __FT_ARG2(param2)		\
+	    : __FT_CLOBBER_LIST2);				\
+	} while (0);
+
+
+#define ft_event3(id, callback, param, param2, param3)		\
+	do {							\
+		long __ft_tmp1, __ft_tmp2;			\
+        __asm__ __volatile__(                                   \
+            "1: jmp 2f                                    \n\t" \
 	    " subl $4, %%esp                              \n\t" \
-            " movl $" #id  ", (%%esp)                     \n\t" \
+            " movl $" #id  ", %%eax                       \n\t" \
+	    " movl %2, (%%esp)                            \n\t" \
 	    " call " #callback "                          \n\t" \
 	    " addl $4, %%esp                              \n\t" \
             ".section __event_table, \"aw\"               \n\t" \
             ".long " #id  ", 0, 1b, 2f                    \n\t" \
             ".previous                                    \n\t" \
             "2:                                           \n\t" \
-        : :  : __FT_CLOBBER_LIST)
-
-#define ft_event1(id, callback, param)                          \
-        __asm__ __volatile__(                                   \
-            "1: jmp 2f                                    \n\t" \
-	    " subl $8, %%esp                              \n\t" \
-	    " movl %0, 4(%%esp)                           \n\t" \
-            " movl $" #id  ", (%%esp)                     \n\t" \
-	    " call " #callback "                          \n\t" \
-	    " addl $8, %%esp                              \n\t" \
-            ".section __event_table, \"aw\"               \n\t" \
-            ".long " #id  ", 0, 1b, 2f                    \n\t" \
-            ".previous                                    \n\t" \
-            "2:                                           \n\t" \
-        : : __FT_ARG(param)  : __FT_CLOBBER_LIST)
-
-#define ft_event2(id, callback, param, param2)                  \
-        __asm__ __volatile__(                                   \
-            "1: jmp 2f                                    \n\t" \
-	    " subl $12, %%esp                             \n\t" \
-	    " movl %1, 8(%%esp)                           \n\t" \
-	    " movl %0, 4(%%esp)                           \n\t" \
-            " movl $" #id  ", (%%esp)                     \n\t" \
-	    " call " #callback "                          \n\t" \
-	    " addl $12, %%esp                             \n\t" \
-            ".section __event_table, \"aw\"               \n\t" \
-            ".long " #id  ", 0, 1b, 2f                    \n\t" \
-            ".previous                                    \n\t" \
-            "2:                                           \n\t" \
-        : :  __FT_ARG(param),  __FT_ARG(param2)  : __FT_CLOBBER_LIST)
-
-
-#define ft_event3(id, callback, p, p2, p3)                      \
-        __asm__ __volatile__(                                   \
-            "1: jmp 2f                                    \n\t" \
-	    " subl $16, %%esp                             \n\t" \
-	    " movl %2, 12(%%esp)                          \n\t" \
-	    " movl %1, 8(%%esp)                           \n\t" \
-	    " movl %0, 4(%%esp)                           \n\t" \
-            " movl $" #id  ", (%%esp)                     \n\t" \
-	    " call " #callback "                          \n\t" \
-	    " addl $16, %%esp                             \n\t" \
-            ".section __event_table, \"aw\"               \n\t" \
-            ".long " #id  ", 0, 1b, 2f                    \n\t" \
-            ".previous                                    \n\t" \
-            "2:                                           \n\t" \
-        : :  __FT_ARG(p),  __FT_ARG(p2), __FT_ARG(p3)  : __FT_CLOBBER_LIST)
+	    : __FT_TMP1(__ft_tmp1), __FT_TMP2(__ft_tmp2)	\
+	    : __FT_ARG1(param), __FT_ARG2(param2), __FT_ARG3(param3)	\
+	    : __FT_CLOBBER_LIST3);				\
+	} while (0);
 
 static inline unsigned long long ft_read_tsc(void)
 {
